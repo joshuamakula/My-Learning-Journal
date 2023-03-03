f8e9f050710a10d0687129776316e185e855512e
Joshua Makula <makulajosh@gmail.com>
2023-03-03 15:44:09 +0300

    make the header reusable

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const viewMore = document.getElementById('view-more')
 
-viewMore.addEventListener('click', function(){
-    const view = document.getElementsByClassName('view')
-    
-    for( let show of view){
-        if(show.style.display === 'none'){
-            show.style.display = 'block'
-            viewMore.textContent = 'View Less'
-        } else {
-            show.style.display = 'none'
-            viewMore.textContent = 'View More'
+if(viewMore){
+    viewMore.addEventListener('click', function(){
+        const view = document.getElementsByClassName('view')
+        
+        for( let show of view){
+            if(show.style.display === 'none'){
+                show.style.display = 'block'
+                viewMore.textContent = 'View Less'
+            } else {
+                show.style.display = 'none'
+                viewMore.textContent = 'View More'
+            }
         }
-    }
-})
+    })
+}
+
+const headerHtml = document.getElementById('header')
+
+headerHtml.innerHTML += `
+    <div class="container my-header">
+        <img src="./images/logo.png" alt="My learning journal" class="logo" />
+        <ul class="my-list">
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./about.html">about me</a></li>
+        </ul>
+    </div>
+`
